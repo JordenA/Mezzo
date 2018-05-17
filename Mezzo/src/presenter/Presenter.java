fd1da5010dd00c5c70471a5e55c5214489001e60
Tuval Lifshitz <tuval.lif@gmail.com>
2018-05-17 10:17:01 +0300

    started working on the algorithem

--- Mezzo/src/presenter/Presenter.java
+++ Mezzo/src/presenter/Presenter.java
@@ -6,6 +6,7 @@ import view.View;
 public class Presenter {
 	private View view;
 	private Model model;
+	
 	public Presenter(View view, Model model) {
 	this.view = view;
 	this.model = model;
---
+++ Mezzo/src/test/Test.java
@@ -0,0 +1,13 @@
+package test;
+
+import algorithm.Algorithm;
+import algorithm.alphaAlgo;
+
+public class Test {
+	
+	public void testering() {
+		Algorithm alg = new alphaAlgo();
+		alg.createFirstPool();
+	}
+
+}
