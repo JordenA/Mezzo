ac14920167f10fd9d393032b9a54896cc7348de7
Tuval Lifshitz <tuval.lif@gmail.com>
2018-05-21 17:15:51 +0300

    extra unsaved

--- Mezzo/src/DNA/MoodChunk.java
+++ Mezzo/src/DNA/MoodChunk.java
@@ -2,7 +2,7 @@ package DNA;
 
 public class MoodChunk extends  AbstractChunk {
 
-	MoodChunk(String name, String score) {
+	public MoodChunk(String name, String score) {
 		super(name, score);
 		// TODO Auto-generated constructor stub
 	}
