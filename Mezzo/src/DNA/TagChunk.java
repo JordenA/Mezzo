c1d841417bfdcf3828d7a80b6218bf26eb38eece
Tuval Lifshitz <tuval.lif@gmail.com>
2018-05-23 18:21:36 +0300

    tagchunk added, tag reader works

---
+++ Mezzo/src/DNA/TagChunk.java
@@ -0,0 +1,10 @@
+package DNA;
+
+public class TagChunk extends AbstractChunk {
+
+	public TagChunk(String name, String score) {
+		super(name, score);
+		// TODO Auto-generated constructor stub
+	}
+
+}
